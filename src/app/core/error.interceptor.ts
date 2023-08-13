c3ea0278d157e01951382a6c205d4a34e10ca980
Vadym Ostapchuk <vadoost@gmail.com>
2023-08-13 18:45:16 +0300

    add error interceptor

---
+++ src/app/core/error.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private snackBar: MatSnackBar) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'An unknown error occurred';
+        if (error.error instanceof ErrorEvent) {
+          // Client-side error
+          errorMessage = `Error: ${error.error.message}`;
+        } else {
+          // Server-side error
+          errorMessage = `Error Code: ${error.status}\nMessage: ${error.error.message}`;
+        }
+        console.error(errorMessage);
+
+        // Show snackbar with the error message
+        this.snackBar.open(errorMessage, 'Close', { duration: 5000 });
+
+        // Rethrow the error to propagate it to the subscriber
+        return throwError(errorMessage);
+      })
+    );
+  }
+}

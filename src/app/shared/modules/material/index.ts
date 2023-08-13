deb7de95dbcd030a68f51c3694e1e6810d3b6c66
Vadym Ostapchuk <vadoost@gmail.com>
2023-08-13 18:45:45 +0300

    add material modules

---
+++ src/app/shared/modules/material/index.ts
@@ -0,0 +1,21 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+
+export const material = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatIconModule,
+  MatTooltipModule,
+  MatCardModule,
+  MatProgressBarModule,
+  MatSnackBarModule,
+  MatInputModule,
+];
